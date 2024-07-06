8c1aceb0e6a0ccb561515638fd7314a23ae4886d
Samir Mahmud <samir93bj@gmail.com>
2024-07-06 15:52:13 -0300

    chore: Update npm dependencies and add @nestjs/mapped-types package

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
+  const logger = new Logger('Main-Gateway');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(envs.port);
+
+  logger.log(`Server is running on port ${envs.port}`);
 }
 bootstrap();
