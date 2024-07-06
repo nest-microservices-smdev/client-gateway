147ce943f3be2c11c58f780b34b1bd45823639ff
Samir Mahmud <samir93bj@gmail.com>
2024-07-06 17:13:45 -0300

    chore: Add microservice configuration for products module

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,11 +3,15 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
+  PRODUCTS_MICROSERVICE_URL: string;
+  PRODUCTS_MICROSERVICE_PORT: number;
 }
 
 const envVarsSchema = joi
   .object({
     PORT: joi.number().required().default(3000),
+    PRODUCTS_MICROSERVICE_URL: joi.string().required(),
+    PRODUCTS_MICROSERVICE_PORT: joi.number().required(),
   })
   .unknown(true);
 
@@ -19,4 +23,6 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
+  productsMicroserviceUrl: envVars.PRODUCTS_MICROSERVICE_URL,
+  productsMicroservicePort: envVars.PRODUCTS_MICROSERVICE_PORT,
 };
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { PRODUCT_SERVICE, envs } from '../config';
 
 @Module({
   controllers: [ProductsController],
   providers: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: PRODUCT_SERVICE,
+        transport: Transport.TCP,
+        options: {
+          host: envs.productsMicroserviceUrl,
+          port: envs.productsMicroservicePort,
+        },
+      },
+    ]),
+  ],
 })
 export class ProductsModule {}
