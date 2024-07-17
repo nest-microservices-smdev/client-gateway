b0ede6adf260aee1a8f474eeebd9c99e3985c71a
Samir Mahmud <samir93bj@gmail.com>
2024-07-17 16:04:32 -0300

    refactor: Update CreateOrderDto to include validation for items array

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,29 +1,11 @@
-import {
-  IsBoolean,
-  IsEnum,
-  IsNumber,
-  IsOptional,
-  IsPositive,
-} from 'class-validator';
-
-import { OrderStatus, OrderStatusList } from '../enum/order.enum';
+import { ArrayMinSize, IsArray, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { OrderItemDto } from './order-item.dto';
 
 export class CreateOrderDto {
-  @IsNumber()
-  @IsPositive()
-  totalAmount: number;
-
-  @IsNumber()
-  @IsPositive()
-  totalItems: number;
-
-  @IsEnum(OrderStatusList, {
-    message: `Status must be one of the following values: ${OrderStatusList}`,
-  })
-  @IsOptional()
-  status = OrderStatus.PENDING;
-
-  @IsBoolean()
-  @IsOptional()
-  paid = false;
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items: any;
 }
