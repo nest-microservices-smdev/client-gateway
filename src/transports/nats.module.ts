14c2e1459f6acd11e9ce2cdd8e64211ec3247e43
Samir Mahmud <samir93bj@gmail.com>
2024-07-19 18:02:07 -0300

    refactor: Update README and config files for NATS integration

--- src/transports/nats.module.ts
+++ src/transports/nats.module.ts
@@ -1,18 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 
-import { envs, ORDER_SERVICE, PRODUCT_SERVICE } from 'src/config';
+import { envs, NATS_SERVICE } from 'src/config';
 
 const natsServices = ClientsModule.register([
   {
-    name: PRODUCT_SERVICE,
-    transport: Transport.NATS,
-    options: {
-      servers: envs.natsServers,
-    },
-  },
-  {
-    name: ORDER_SERVICE,
+    name: NATS_SERVICE,
     transport: Transport.NATS,
     options: {
       servers: envs.natsServers,
