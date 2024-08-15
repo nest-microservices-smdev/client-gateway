411a10d1de2b04b3f2811c0284fffb4813a7f8ae
samir93bj <samir93bj@gmail.com>
2024-08-15 00:22:47 -0300

    refactor: Add AuthGuard to verifyUser endpoint in AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,21 @@
-import { Body, Controller, Get, Inject, Logger, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Logger,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
 
 import { NATS_SERVICE } from '../config';
 import { CreateUserDto } from './dto';
 import { LoginUserDto } from './dto/login-user.dto';
+import { AuthGuard } from './guards/auth.guard';
+import { Token, User } from './decorators';
+import { CurrentUser } from './interfaces/current-user.interface';
 
 @Controller('auth')
 export class AuthController {
@@ -42,12 +53,16 @@ export class AuthController {
     }
   }
 
+  @UseGuards(AuthGuard)
   @Get('verify')
-  async verifyUser() {
+  async verifyUser(@User() user: CurrentUser, @Token() token: string) {
     try {
+      /*
       return firstValueFrom(
-        this.authClient.send({ cmd: 'auth.verify.user' }, {}),
+        this.authClient.send({ cmd: 'auth.verify.user' }, req),
       );
+      */
+      return { user, token };
     } catch (error) {
       throw new RpcException(error);
     }
